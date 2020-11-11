dc694bab2b94a39e710a4d5360fe5e8eead757fb
vladislavkostromin <61909342+vladislavkostromin@users.noreply.github.com>
2020-11-11 16:49:51 +0500

    pravki

--- js/main.js
+++ js/main.js
@@ -503,6 +503,22 @@ $(function () {
   // });
 
 
+  $(document).ready(function() {
+    $('.reviews-61__photos').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+      },
+      image: {
+        
+      }
+    });
+  });
 
 
   if ($('#map-contact')) {
